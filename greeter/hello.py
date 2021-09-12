aad504d3562583bee6475f28fb552201f25e357e
HerrCraziDev <herrcrazi@gmail.com>
2021-09-12 14:50:47 +0200

    Sample pytest boilerplate helloworld

---
+++ greeter/hello.py
@@ -0,0 +1,17 @@
+
+
+class Hello:
+
+    def __init__(self, greet: str = "World"):
+        self.name = greet
+    
+    def greet(self):
+        return f"Hello {self.name}!"
+    
+    def set_name(self, name: str):
+        self.name = name
+
+
+if (__name__ == "__main__"):
+    greeter = Hello()
+    print(greeter.greet())
---
+++ tests/test_hello.py
@@ -0,0 +1,24 @@
+
+import pytest
+from greeter.hello import Hello
+
+@pytest.mark.unit
+def test_hello():
+    hello = Hello()
+    assert hello.name == "World"
+
+    hello = Hello("user")
+    assert hello.name == "user"
+
+@pytest.mark.unit
+def test_hello_greeter():
+    hello = Hello()
+    assert hello.greet() == "Hello World!"
+
+@pytest.mark.unit
+def test_hello_set_name():
+    hello = Hello()
+    name = "toto"
+    hello.set_name(name)
+    assert hello.name == name
+    assert hello.greet() == f"Hello {name}!"
