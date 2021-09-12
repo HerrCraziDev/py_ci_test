bd0599028d352364f0a30cc32ea8b4d46e4d2a7d
HerrCraziDev <herrcrazi@gmail.com>
2021-09-12 15:49:51 +0200

    black + flake8

--- greeter/hello.py
+++ greeter/hello.py
@@ -1,17 +1,14 @@
-
-
 class Hello:
-
     def __init__(self, greet: str = "World"):
         self.name = greet
-    
+
     def greet(self):
         return f"Hello {self.name}!"
-    
+
     def set_name(self, name: str):
         self.name = name
 
 
-if (__name__ == "__main__"):
+if __name__ == "__main__":
     greeter = Hello()
     print(greeter.greet())
